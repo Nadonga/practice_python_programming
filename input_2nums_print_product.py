e645d51c8f04a106db1ac2f3ce9216cdc04a31d2
Nadonga <nadongayael@gmail.com>
2026-03-08 17:07:24 +0800

    created program that asks user for 2 numbers and prints their product

--- input_2nums_print_product.py
+++ input_2nums_print_product.py
@@ -1,5 +1,5 @@
 #Prog04: Create a program that ask user to input 2 numbers. Print the product of the two numbers.
 
 number = float(input("Enter first number: "))
-number /= float(input("Enter second number: "))
-print(f"Quotient: ",number)
+number *= float(input("Enter second number: "))
+print(f"Product: ",number)
