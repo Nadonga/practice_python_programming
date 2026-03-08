fbb3d049fcffdb8979b584e0a25a8e1ebf0125b8
Nadonga <nadongayael@gmail.com>
2026-03-08 16:00:49 +0800

    created a program that asks the user to input two numbers and prints the bigger one

---
+++ input_2nums_print_bigger.py
@@ -0,0 +1,8 @@
+#Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: ")) 
+if num1 > num2 :
+    print("First number is bigger")
+else:
+    print("Second number is bigger")
