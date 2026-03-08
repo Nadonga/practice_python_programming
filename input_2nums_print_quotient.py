7f9fc201a02452380ce216947c8dbe9768933353
Nadonga <nadongayael@gmail.com>
2026-03-08 16:54:35 +0800

    created program that asks user for 2 numbers and prints their quotient with decimal point

---
+++ input_2nums_print_quotient.py
@@ -0,0 +1,6 @@
+#Prog05: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
+
+
+number = float(input("Enter first number: "))
+number /= float(input("Enter second number: "))
+print(f"Quotient: ",number)
