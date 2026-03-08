014155e7982f5fffeec4ef8053c87541aeb72189
Nadonga <nadongayael@gmail.com>
2026-03-08 17:10:19 +0800

    Delete file input_2nums_print_product.py

--- input_2nums_print_product.py
+++
@@ -1,5 +0,0 @@
-#Prog04: Create a program that ask user to input 2 numbers. Print the product of the two numbers.
-
-number = float(input("Enter first number: "))
-number *= float(input("Enter second number: "))
-print(f"Product: ",number)
