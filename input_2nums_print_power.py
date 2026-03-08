64a389cb93f82063a20aa7bcce729f0a9f55d993
Nadonga <nadongayael@gmail.com>
2026-03-08 17:21:48 +0800

    created program that asks user for 2 numbers and prints first raised to second

---
+++ input_2nums_print_power.py
@@ -0,0 +1,5 @@
+#Prog06: Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
+
+number = float(input("Enter first number: "))
+number **= float(input("Enter second number: "))
+print(f"Power: ",number)
