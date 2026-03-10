2fe7ae91f8ed0885c35c9dc1fd45bb598dc4a9e3
Nadonga <nadongayael@gmail.com>
2026-03-10 09:05:08 +0800

    created program that asks user for 2 numbers and prints smaller

--- input_2nums_print_smaller.py
+++ input_2nums_print_smaller.py
@@ -1,1 +1,8 @@
 #Prog01: Create a program that ask user to input 2 numbers. Print the smaller number.
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: ")) 
+if num1 < num2 :
+    print("First number is smaller")
+else:
+    print("Second number is smaller")
