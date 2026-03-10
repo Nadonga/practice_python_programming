9b5f56dfbaabf25e399115e3d3aa2adfe7288ee7
Nadonga <nadongayael@gmail.com>
2026-03-10 09:25:36 +0800

    created program that asks user for 2 numbers and prints quotient without decimal

---
+++ input_2nums_print_quotient.py
@@ -0,0 +1,5 @@
+#Prog04: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
+
+number = float(input("Enter first number: "))
+number //= float(input("Enter second number: "))
+print(f"Quotient: ",number)
