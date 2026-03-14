17c3c68297edc5b458e3a93fd4ffa44270213399
Nadonga <nadongayael@gmail.com>
2026-03-14 11:20:56 +0800

    created a program that asks user to input fullname and prints number of characters

---
+++ input_name_char_count.py
@@ -0,0 +1,5 @@
+#Prog08: Create a program that ask the user to input their fullname. Print the number of characters in the input.
+
+statement =(input( "Enter any statement: "))
+length = len(statement)
+print(length)
