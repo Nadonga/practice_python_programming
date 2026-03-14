312208f31a4d57a3bd6ea350beaf82a85921fa09
Nadonga <nadongayael@gmail.com>
2026-03-14 10:50:03 +0800

    created a program that asks user to input numbers continuously until invalid and displays numbers from lowest to highest using sort()

---
+++ input_num_sort_lowest_highest.py
@@ -0,0 +1,13 @@
+#Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from lowest to highest. Clue: sort() function
+
+numbers = []
+
+while True:
+    try:
+        num = int(input("Enter a number: "))
+        numbers.append(num)
+    except:
+        break
+ 
+numbers.sort()
+print(numbers)
