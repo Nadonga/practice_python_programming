e7582bd1d4f98e0e9d3d9f97f68050f412679f86
Nadonga <nadongayael@gmail.com>
2026-03-14 11:19:28 +0800

    created a program that asks user to input statement and prints number of words

---
+++ input_statement_word_count.py
@@ -0,0 +1,6 @@
+#Prog07: Create a program that ask the user to input a complete statement. Print the number of words in the input.
+
+statement =(input( "Enter any statement: "))
+words = statement.split()
+length = len(words)
+print(length)
