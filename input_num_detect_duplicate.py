97f51080b664deaa21a5251c7f2bf58f87fb615a
Nadonga <nadongayael@gmail.com>
2026-03-14 10:46:06 +0800

    created a program that asks user to input numbers continuously until invalid and displays Unique or Duplicate after each input

---
+++ input_num_detect_duplicate.py
@@ -0,0 +1,14 @@
+#Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display "Unique" after input when the inputted number don't have duplicate. Display "Duplicate" after input when the inputted number have duplicate.
+
+numbers = []
+
+while True:
+    try:
+        num = int(input("Enter a number: "))
+        numbers.append(num)
+        if numbers.count(num) == 1:
+            print("Unique")
+        else:
+            print("Duplicate")
+    except:
+        break
