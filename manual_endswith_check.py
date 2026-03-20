be7071185e7adfee7df33b6fad960884b8a5649a
Nadonga <nadongayael@gmail.com>
2026-03-20 10:12:25 +0800

    Created a program that checks if string ends with given suffix without using endswith() function.

---
+++ manual_endswith_check.py
@@ -0,0 +1,9 @@
+#Prog05. endswith() check if the string end part matches the function parameter. Create a program that do the same functionality without using endswith() function.
+
+text = input("Enter a string: ")
+suffix = input("Enter suffix: ")
+
+if len(suffix) <= len(text) and text[-len(suffix):] == suffix:
+    print(True)
+else:
+    print(False)
