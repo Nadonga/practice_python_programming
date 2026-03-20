2d59bfe766026ab7ceb5f83b0964155bc1cadfa4
Nadonga <nadongayael@gmail.com>
2026-03-20 09:18:33 +0800

    Created a program that takes fullname input and prints each character in reverse casing.

---
+++ reverse_fullname_casing.py
@@ -0,0 +1,5 @@
+#Prog06: Create a program that ask the user to input their fullname in incorrect casing. Print each character of the input in reverse casing.
+
+name = input("Enter a name: ")
+reversed = name.swapcase()
+print(reversed)
